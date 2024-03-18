4ec59109ee5ab930bda6a99aea5492522e0762cd
schwarper <75811921+schwarper@users.noreply.github.com>
2024-03-18 03:17:06 +0300

    0.0.2
    - Added a config setting to choose whether to use the slot2-slot3 system or reload after a deagle shot.
    - Added a config setting to enable or disable infinite clip for deagle.

--- cs2-deagleoneshot.cs
+++ cs2-deagleoneshot.cs
@@ -1,52 +1,93 @@
 using CounterStrikeSharp.API.Core;
+using System.Text.Json.Serialization;
 using static CounterStrikeSharp.API.Core.Listeners;
 
 namespace DeagleOneShot;
 
-public class DeagleOneShot : BasePlugin
+public class DeagleOneShotConfig : BasePluginConfig
+{
+    [JsonPropertyName("deagle_reload")] public bool Reload { get; set; } = true;
+    [JsonPropertyName("deagle_infinite_clip")] public bool InfiniteClip { get; set; } = true;
+}
+
+public class DeagleOneShot : BasePlugin, IPluginConfig<DeagleOneShotConfig>
 {
     public override string ModuleName => "Deagle One Shot";
-    public override string ModuleVersion => "0.0.1";
+    public override string ModuleVersion => "0.0.2";
     public override string ModuleAuthor => "schwarper";
+
+    public DeagleOneShotConfig Config { get; set; } = new();
+
     public override void Load(bool hotReload)
     {
-        RegisterListener<OnEntitySpawned>((entity) =>
-        {
-            if (entity.DesignerName != "weapon_deagle")
-            {
-                return;
-            }
-
-            CBasePlayerWeapon weapon = entity.As<CBasePlayerWeapon>();
-
-            if (weapon == null)
-            {
-                return;
-            }
-
-            CCSWeaponBaseVData? weaponData = weapon.As<CCSWeaponBase>().VData;
-
-            if (weaponData != null)
-            {
-                weaponData.MaxClip1 = 1;
-                weaponData.DefaultClip1 = 1;
-                weaponData.PrimaryReserveAmmoMax = 1;
-            }
-
-            weapon.Clip1 = 1;
-            weapon.ReserveAmmo[0] = 1;
-        });
-
         RegisterEventHandler<EventWeaponFire>((@event, info) =>
         {
-            if (@event.Weapon != "weapon_deagle" || @event.Userid.PlayerPawn.Value?.WeaponServices?.ActiveWeapon.Value is not CBasePlayerWeapon activeweapon)
+            if (@event.Weapon != "weapon_deagle")
             {
                 return HookResult.Continue;
             }
 
-            activeweapon.ReserveAmmo[0] = 2;
+            CCSPlayerController player = @event.Userid;
+
+            if (player == null || !player.IsValid)
+            {
+                return HookResult.Continue;
+            }
+
+            if (!Config.Reload)
+            {
+                player.ExecuteClientCommand("slot3");
+
+                AddTimer(0.1f, () =>
+                {
+                    player.ExecuteClientCommand("slot2");
+                });
+            }
+
+            if (Config.InfiniteClip)
+            {
+                if (player.PlayerPawn.Value?.WeaponServices?.ActiveWeapon.Value is not CBasePlayerWeapon activeweapon)
+                {
+                    return HookResult.Continue;
+                }
+
+                activeweapon.ReserveAmmo[0] += 1;
+            }
 
             return HookResult.Continue;
         });
+    }
+
+    public void OnConfigParsed(DeagleOneShotConfig config)
+    {
+        if (config.Reload)
+        {
+            RegisterListener<OnEntitySpawned>((entity) =>
+            {
+                if (entity.DesignerName != "weapon_deagle")
+                {
+                    return;
+                }
+
+                CBasePlayerWeapon weapon = entity.As<CBasePlayerWeapon>();
+
+                if (weapon == null)
+                {
+                    return;
+                }
+
+                CCSWeaponBaseVData? weaponData = weapon.As<CCSWeaponBase>().VData;
+
+                if (weaponData != null)
+                {
+                    weaponData.MaxClip1 = 1;
+                    weaponData.DefaultClip1 = 1;
+                }
+
+                weapon.Clip1 = 1;
+            });
+        }
+
+        Config = config;
     }
 }
